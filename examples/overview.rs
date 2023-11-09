a2dc7b45bd2df55d9ecabeb39197575a4a32285e
CosmicHorror <CosmicHorrorDev@pm.me>
2023-11-08 20:39:01 -0700

    Get things back to compiling and passing (#38)

--- examples/overview.rs
+++ examples/overview.rs
@@ -4,16 +4,22 @@ fn main() {
     let steamdir = SteamDir::locate().unwrap();
     println!("Steam Dir - {:?}", steamdir.path());
 
-    for library in steamdir.libraries().unwrap() {
-        println!("    Library - {:?}", library.path());
-        for app in library.apps() {
-            match app {
-                Some(app) => println!(
-                    "        App {} - {}",
-                    app.app_id,
-                    app.name.as_deref().unwrap_or("<no-name>")
-                ),
-                None => println!("        There was a FAILURE!"),
+    // TODO: use `anyhow` to make error handling here simpler
+    for maybe_library in steamdir.libraries().unwrap() {
+        match maybe_library {
+            Err(err) => eprintln!("Failed reading library: {err}"),
+            Ok(library) => {
+                println!("    Library - {:?}", library.path());
+                for app in library.apps() {
+                    match app {
+                        Ok(app) => println!(
+                            "        App {} - {}",
+                            app.app_id,
+                            app.name.as_deref().unwrap_or("<no-name>")
+                        ),
+                        Err(err) => println!("        Failed reading app: {err}"),
+                    }
+                }
             }
         }
     }
