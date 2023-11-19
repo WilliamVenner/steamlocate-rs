68d1c0ab215aced92467e8e080950ae1ce9da3b3
CosmicHorror <CosmicHorrorDev@pm.me>
2023-11-19 16:42:41 -0700

    Run `cargo test` in CI (#42)
    * Run `cargo test` in CI
    
    * `#[ignore]` a couple lingering doctests

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@
 //! # Examples
 //!
 //! ### Locate the installed Steam directory
-//! ```rust
+//! ```rust,ignore
 //! extern crate steamlocate;
 //! use steamlocate::SteamDir;
 //!
@@ -165,7 +165,7 @@ pub mod test_helpers;
 /// If you'd like to dispose of the cache or get uncached results, just instantiate a new `SteamDir`.
 ///
 /// # Example
-/// ```rust
+/// ```rust,ignore
 /// # use steamlocate::SteamDir;
 /// let steamdir = SteamDir::locate();
 /// println!("{:#?}", steamdir.unwrap());
