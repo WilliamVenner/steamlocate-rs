d9f998a0dd15cd2d7e10c32f7fd1aa64e0a5f876
CosmicHorror <CosmicHorrorDev@pm.me>
2023-12-22 20:54:05 -0700

    Alias `lastupdated` for `last_updated` (#61)
    * Add failing test
    
    * Alias `last_updated` to `lastupdated` as well

---
+++ src/tests/tests.rs
@@ -0,0 +1,18 @@
+use crate::tests::{
+    helpers::{SampleApp, TempSteamDir},
+    TestResult,
+};
+
+// Context: https://github.com/WilliamVenner/steamlocate-rs/issues/58
+#[test]
+fn app_lastupdated_casing() -> TestResult {
+    let sample_app = SampleApp::Resonite;
+    let temp_steam_dir = TempSteamDir::builder().app(sample_app.into()).finish()?;
+    let steam_dir = temp_steam_dir.steam_dir();
+
+    let (app, _library) = steam_dir.find_app(sample_app.id())?.unwrap();
+    // Last updated _should_ be `Some(_)` for this app even though it uses lowercase casing
+    let _ = app.last_updated.unwrap();
+
+    Ok(())
+}
