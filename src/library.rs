1df35c9af5f38e4463f4d3da8d44d1499de73844
Cosmic Horror <CosmicHorrorDev@pm.me>
2024-10-22 19:59:34 -0600

    docs: Sprinkle more documentation around

--- src/library.rs
+++ src/library.rs
@@ -1,3 +1,9 @@
+//! Functionality related to Steam [`Library`]s and related types
+//!
+//! [`Library`]s are obtained from either [`SteamDir::libraries()`][super::SteamDir::libraries],
+//! [`SteamDir::find_app()`][super::SteamDir::find_app], or located manually with
+//! [`Library::from_dir()`].
+
 use std::{
     fs,
     path::{Path, PathBuf},
@@ -74,6 +80,9 @@ pub(crate) fn parse_library_paths(path: &Path) -> Result<Vec<PathBuf>> {
     Ok(paths)
 }
 
+/// An [`Iterator`] over a Steam installation's [`Library`]s
+///
+/// Returned from calling [`SteamDir::libraries()`][super::SteamDir::libraries]
 pub struct Iter {
     paths: std::vec::IntoIter<PathBuf>,
 }
@@ -100,6 +109,7 @@ impl ExactSizeIterator for Iter {
     }
 }
 
+/// A steam library containing various installed [`App`]s
 #[derive(Clone, Debug)]
 pub struct Library {
     path: PathBuf,
@@ -107,6 +117,11 @@ pub struct Library {
 }
 
 impl Library {
+    /// Attempt to create a [`Library`] directly from its installation directory
+    ///
+    /// You'll typically want to use methods that handle locating the library for you like
+    /// [`SteamDir::libraries()`][super::SteamDir::libraries] or
+    /// [`SteamDir::find_app()`][super::SteamDir::find_app].
     pub fn from_dir(path: &Path) -> Result<Self> {
         // Read the manifest files at the library to get an up-to-date list of apps since the
         // values in `libraryfolders.vdf` may be stale
@@ -131,14 +146,51 @@ impl Library {
         })
     }
 
+    /// Returns the path to the library's installation directory
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use steamlocate::__private_tests::prelude::*;
+    /// # let temp_steam_dir = expect_test_env();
+    /// # let steam_dir = temp_steam_dir.steam_dir();
+    /// # let library = steam_dir.libraries().unwrap().next().unwrap().unwrap();
+    /// # /*
+    /// let library = /* Somehow get a library */;
+    /// # */
+    /// let path = library.path();
+    /// assert!(path.join("steamapps").is_dir());
+    /// ```
     pub fn path(&self) -> &Path {
         &self.path
     }
 
+    /// Returns the full list of Application IDs located within this library
     pub fn app_ids(&self) -> &[u32] {
         &self.apps
     }
 
+    /// Attempts to return the [`App`] identified by `app_id`
+    ///
+    /// Returns [`None`] if the app isn't located within this library. Otherwise it attempts to
+    /// return metadata for the installed app
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use steamlocate::__private_tests::prelude::*;
+    /// # let temp_steam_dir = expect_test_env();
+    /// # let steam_dir = temp_steam_dir.steam_dir();
+    /// # let library = steam_dir.libraries()?.next().unwrap()?;
+    /// const GMOD: u32 = 4_000;
+    /// # /*
+    /// let library = /* Somehow get a library */;
+    /// # */
+    /// let gmod = library.app(GMOD).expect("Of course we have gmod")?;
+    /// assert_eq!(gmod.app_id, GMOD);
+    /// assert_eq!(gmod.name.unwrap(), "Garry's Mod");
+    /// # Ok::<_, TestError>(())
+    /// ```
     pub fn app(&self, app_id: u32) -> Option<Result<App>> {
         self.app_ids().iter().find(|&&id| id == app_id).map(|&id| {
             let manifest_path = self
@@ -149,6 +201,30 @@ impl Library {
         })
     }
 
+    /// Returns an [`Iterator`] over all of the [`App`]s contained in this library
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use steamlocate::__private_tests::prelude::*;
+    /// # let temp_steam_dir = expect_test_env();
+    /// # let steam_dir = temp_steam_dir.steam_dir();
+    /// # let library = steam_dir.libraries()?.next().unwrap()?;
+    /// # /*
+    /// let library = /* Somehow get a library */;
+    /// # */
+    /// let total_size: u64 = library
+    ///     .apps()
+    ///     .filter_map(Result::ok)
+    ///     .filter_map(|app| app.bytes_downloaded)
+    ///     .sum();
+    /// println!(
+    ///     "Library {} takes up {} bytes",
+    ///     library.path().display(), total_size,
+    /// );
+    /// # assert_eq!(total_size, 30804429728);
+    /// # Ok::<_, TestError>(())
+    /// ```
     pub fn apps(&self) -> app::Iter {
         app::Iter::new(self)
     }
