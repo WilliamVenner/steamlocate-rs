3c5f15b1e8a50bdf051e14944add85b21ad7ee89
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-08-01 21:35:05 -0600

    refactor: tweak re-exports a bit

--- src/locate/mod.rs
+++ src/locate/mod.rs
@@ -1,34 +1,31 @@
-#[cfg(target_os = "linux")]
-use std::path::PathBuf;
-
 use crate::Result;
 
 #[cfg(target_os = "linux")]
-pub mod linux;
+mod linux;
 #[cfg(target_os = "linux")]
 use crate::locate::linux::locate_steam_dir_helper;
 
 #[cfg(target_os = "windows")]
-pub mod windows;
+mod windows;
 #[cfg(target_os = "windows")]
 use crate::locate::windows::locate_steam_dir_helper;
 
 #[cfg(target_os = "macos")]
-pub mod macos;
+mod macos;
 #[cfg(target_os = "macos")]
 use crate::locate::macos::locate_steam_dir_helper;
 
 #[cfg(target_os = "linux")]
-pub fn locate_steam_dir() -> Result<Vec<PathBuf>> {
+pub fn locate_steam_dir() -> Result<Vec<std::path::PathBuf>> {
     locate_steam_dir_helper()
 }
 #[cfg(not(target_os = "linux"))]
-pub fn locate_steam_dir() -> Result<Vec<PathBuf>> {
+pub fn locate_steam_dir() -> Result<Vec<std::path::PathBuf>> {
     locate_steam_dir_helper().map(|path| vec![path])
 }
 
 #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
-fn locate_steam_dir_helper() -> Result<PathBuf> {
+fn locate_steam_dir_helper() -> Result<std::path::PathBuf> {
     use crate::error::{Error, LocateError};
     Err(Error::locate(LocateError::Unsupported))
 }
