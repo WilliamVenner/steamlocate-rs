d7a249667ceeaadffba285547106e8d809507ae9
Cosmic Horror <CosmicHorrorDev@pm.me>
2026-07-22 11:01:45 -0600

    deps: `wasm-bindgen-test` only needed on `wasm`

--- tests/wasm.rs
+++ tests/wasm.rs
@@ -1,13 +1,6 @@
-use wasm_bindgen_test::wasm_bindgen_test;
-
-#[wasm_bindgen_test]
-#[cfg_attr(
-    not(any(target_os = "windows", target_os = "macos", target_os = "linux")),
-    ignore = "Needs `locate` feature"
-)]
+// TODO: add tests that we can load a steam install using `::from_dir()`
+#[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
+#[wasm_bindgen_test::wasm_bindgen_test]
 fn locate() {
-    #[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
-    unreachable!("Don't run ignored tests silly");
-    #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
     let _ = steamlocate::locate().unwrap_err();
 }
